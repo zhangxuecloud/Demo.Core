2e547ed5c5932bd736d8f156f54cbde945865cb9
2894942787@qq.com <2894942787@qq.com>
2020-10-07 14:08:11 +0800

    homeindex

---
+++ Demo.Core.ApiServer/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.Core.ApiServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Index()
+        {
+            return "hello" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff");
+        }
+    }
+}
